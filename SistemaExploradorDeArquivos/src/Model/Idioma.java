47fece8d1f6fa0274746e525d1e410c3402a4b41
Cauã <cauabertolini@gmail.com>
2025-11-04 08:46:21 -0300

    Ciração das classes Pessoa e Idioma

---
+++ SistemaExploradorDeArquivos/src/Model/Idioma.java
@@ -0,0 +1,13 @@
+package Model;
+
+public class Idioma {
+    String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
--- SistemaExploradorDeArquivos/src/Model/Livro.java
+++ SistemaExploradorDeArquivos/src/Model/Livro.java
@@ -1,4 +1,8 @@
 package Model;
 
+import java.util.ArrayList;
+
 public class Livro {
+
+    private ArrayList<Pessoa> autores;
 }
