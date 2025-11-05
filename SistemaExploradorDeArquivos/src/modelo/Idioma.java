9751269fea1a2c26e85f9c501a7a9a59c064d940
Cauã <cauabertolini@gmail.com>
2025-11-05 08:40:24 -0300

    Aplicação da classe Utilitario.java ás validações de Midia.java

--- SistemaExploradorDeArquivos/src/modelo/Idioma.java
+++ SistemaExploradorDeArquivos/src/modelo/Idioma.java
@@ -1,5 +1,8 @@
 package modelo;
 
+import excecao.CampoVazioOuNuloExcecao;
+import excecao.Utilitario;
+
 public class Idioma {
     String nome;
 
@@ -7,7 +10,9 @@ public class Idioma {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public void setNome(String nome) throws CampoVazioOuNuloExcecao {
+        if (!Utilitario.campoEstaVazioOuNulo(nome)) {
+            this.nome = nome;
+        }
     }
 }
