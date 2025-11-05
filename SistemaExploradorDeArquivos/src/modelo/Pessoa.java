ca55f13fd16834b35d500f0cfe6a67d49ed102b1
Cauã <cauabertolini@gmail.com>
2025-11-05 08:21:53 -0300

    Criação de Getters and Setters Pessoa

--- SistemaExploradorDeArquivos/src/modelo/Pessoa.java
+++ SistemaExploradorDeArquivos/src/modelo/Pessoa.java
@@ -2,5 +2,21 @@ package modelo;
 
 public class Pessoa {
     private String nome;
-    private String tipo;
+    private EtipoPessoa tipo;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
 }
