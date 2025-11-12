83d7e8b5fe6038639e558c4c4bb9e7c1652616bd
Cauã <cauabertolini@gmail.com>
2025-11-12 10:56:16 -0300

    Criação dos métodos: -> Mover Mídia -> Renomear Mídia
    Relevante:
    ListaGenero.Java agora é Listas.java
    Listas.java possui lista de Idiomas.

--- SistemaExploradorDeArquivos/src/controle/ExploradorDeArquivos.java
+++ SistemaExploradorDeArquivos/src/controle/ExploradorDeArquivos.java
@@ -13,81 +13,142 @@ import java.nio.file.Paths;
 
 
 public class ExploradorDeArquivos {
-        Salvamento sv = new Salvamento();
+    Salvamento sv = new Salvamento();
 
-        ListaGenero generos = new ListaGenero();
+    Listas generos = new Listas();
 
-        public boolean criarNovaMidia(String caminho, String nome, float tamanho, double duracao, ETipoArquivo tipoArquivo, Genero genero, Idioma idioma) {
+    public boolean criarNovaMidia(String caminho, String nome, float tamanho, double duracao, ETipoArquivo tipoArquivo, Genero genero, Idioma idioma) {
 
-            String caminhoCompleto = nome + ".tpoo";
-            Midia novaMidia;
+        String caminhoCompleto = nome + ".tpoo";
+        Midia novaMidia;
 
-            try {
-                novaMidia = new Filme(caminhoCompleto, nome, tamanho, duracao, tipoArquivo, genero ,idioma);
-                sv.incluirMidia(novaMidia);
+        try {
+            novaMidia = new Filme(caminhoCompleto, nome, tamanho, duracao, tipoArquivo, genero, idioma);
+            sv.incluirMidia(novaMidia);
 
-                File f = new File(caminhoCompleto);
-                FileOutputStream fos = new FileOutputStream(f);
-                ObjectOutputStream oos = new ObjectOutputStream(fos);
-                oos.writeObject(novaMidia.toString());
+            File f = new File(caminhoCompleto);
+            FileOutputStream fos = new FileOutputStream(f);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(novaMidia.toString());
 
-                oos.close();
+            oos.close();
 
-            } catch (Exception e) {
-                JOptionPane.showMessageDialog(null, e.getMessage());
-                return false;
-            }
-            return true;
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+            return false;
         }
+        return true;
+    }
 
 
-        public boolean criarNovaMidia(String caminho, String nome, float tamanho, double duracao, Genero genero, String autorOuArtista, boolean eLivro) {
+    public boolean criarNovaMidia(String caminho, String nome, float tamanho, double duracao, Genero genero, String autorOuArtista, boolean eLivro) {
 
-            String caminhoCompleto = nome + ".tpoo";
-            Midia novaMidia;
+        String caminhoCompleto = nome + ".tpoo";
+        Midia novaMidia;
 
-            try {
-                if (eLivro) {
-                    novaMidia = new Livro(caminhoCompleto, nome, tamanho, duracao, ETipoArquivo.MP4, genero, autorOuArtista);
-                } else {
-                    novaMidia = new Musica(caminhoCompleto, nome, tamanho, duracao,  ETipoArquivo.MP4, genero, autorOuArtista);
+        try {
+            if (eLivro) {
+                novaMidia = new Livro(caminhoCompleto, nome, tamanho, duracao, ETipoArquivo.MP4, genero, autorOuArtista);
+            } else {
+                novaMidia = new Musica(caminhoCompleto, nome, tamanho, duracao, ETipoArquivo.MP4, genero, autorOuArtista);
+            }
+
+            sv.incluirMidia(novaMidia);
+
+            File f = new File(caminhoCompleto);
+            FileOutputStream fos = new FileOutputStream(f);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(novaMidia);
+
+            oos.close();
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+    public boolean excluirMidia(Midia midia) {
+        try {
+            Path caminho = Paths.get(midia.getCaminho());
+            Files.deleteIfExists(caminho);
+
+            sv.removerMidia(midia);
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+    public boolean alterarMidia(String caminhoArquivo, float tamanho, double duracao, ETipoArquivo eTipoArquivo, Idioma idioma) throws ArquivoNaoExisteExcecao, CampoVazioOuNuloExcecao, CampoMenorOuIgualAZeroExcecao {
+        try {
+            if (Utilitario.arquivoExiste(caminhoArquivo)) {
+                Midia midiaAlterando;
+                Filme filmeAlterando = null;
+
+                try (FileInputStream fis = new FileInputStream(caminhoArquivo);
+                     ObjectInputStream ois = new ObjectInputStream(fis)) {
+
+                    midiaAlterando = (Midia) ois.readObject();
                 }
 
-                sv.incluirMidia(novaMidia);
+                if (midiaAlterando instanceof Filme) {
+                    filmeAlterando = (Filme) midiaAlterando;
+                } else {
+                    return false;
+                }
 
-                File f = new File(caminhoCompleto);
-                FileOutputStream fos = new FileOutputStream(f);
-                ObjectOutputStream oos = new ObjectOutputStream(fos);
-                oos.writeObject(novaMidia);
+                if (tamanho > 0) {
+                    filmeAlterando.setTamanho(tamanho);
+                }
 
-                oos.close();
+                if (duracao > 0) {
+                    filmeAlterando.setDuracao(duracao);
+                }
 
-            } catch (Exception e) {
-                JOptionPane.showMessageDialog(null, e.getMessage());
-                return false;
+                if (idioma != null) {
+                    filmeAlterando.setIdioma(idioma);
+                }
+
+                if (eTipoArquivo != null) {
+                    filmeAlterando.setTipoArquivo(eTipoArquivo);
+                }
+
+                try (FileOutputStream fos = new FileOutputStream(caminhoArquivo);
+                     ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+                    oos.writeObject(midiaAlterando);
+                }
+
+                sv.atualizarMidia(midiaAlterando);
+
+                return true;
             }
-            return true;
+        } catch (ArquivoNaoExisteExcecao a) {
+            JOptionPane.showMessageDialog(null, a.getMessage());
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
         }
+        return false;
+    }
 
-        public boolean excluirMidia(Midia midia) {
-            try {
-                Path caminho = Paths.get(midia.getCaminho());
-                Files.deleteIfExists(caminho);
+    public boolean alterarMidia(String caminhoArquivo, float tamanho, double duracao, ETipoArquivo eTipoArquivo, String autorOuArtista, boolean eLivro) throws ArquivoNaoExisteExcecao, CampoVazioOuNuloExcecao, CampoMenorOuIgualAZeroExcecao {
+        try {
+            if (Utilitario.arquivoExiste(caminhoArquivo)) {
 
-                sv.removerMidia(midia);
+                Midia midiaAlterando;
 
-            } catch (Exception e) {
-                JOptionPane.showMessageDialog(null, e.getMessage());
-                return false;
-            }
-            return true;
-        }
-
-        public boolean alterarMidia(String caminhoArquivo, float tamanho, double duracao, ETipoArquivo eTipoArquivo, Idioma idioma) throws ArquivoNaoExisteExcecao, CampoVazioOuNuloExcecao, CampoMenorOuIgualAZeroExcecao {
-            try {
-                if (Utilitario.arquivoExiste(caminhoArquivo)) {
-                    Midia midiaAlterando;
-                    Filme filmeAlterando = null;
+                if (eLivro) {
+                    Livro livroAlterando = null;
 
                     try (FileInputStream fis = new FileInputStream(caminhoArquivo);
                          ObjectInputStream ois = new ObjectInputStream(fis)) {
@@ -95,144 +156,115 @@ public class ExploradorDeArquivos {
                         midiaAlterando = (Midia) ois.readObject();
                     }
 
-                    if (midiaAlterando instanceof Filme) {
-                        filmeAlterando =  (Filme) midiaAlterando;
+                    if (midiaAlterando instanceof Livro) {
+                        livroAlterando = (Livro) midiaAlterando;
                     } else {
                         return false;
                     }
-                    
+
                     if (tamanho > 0) {
-                        filmeAlterando.setTamanho(tamanho);
+                        livroAlterando.setTamanho(tamanho);
                     }
-                    
+
                     if (duracao > 0) {
-                        filmeAlterando.setDuracao(duracao);
+                        livroAlterando.setDuracao(duracao);
                     }
-                    
-                    if (idioma != null) {
-                        filmeAlterando.setIdioma(idioma);
+
+                    if (!autorOuArtista.isBlank()) {
+                        livroAlterando.setAutor(autorOuArtista);
                     }
 
                     if (eTipoArquivo != null) {
-                        filmeAlterando.setTipoArquivo(eTipoArquivo);
+                        livroAlterando.setTipoArquivo(eTipoArquivo);
                     }
 
-                    try (FileOutputStream fos = new FileOutputStream(caminhoArquivo);
-                    ObjectOutputStream oos = new ObjectOutputStream(fos)) {
-                        oos.writeObject(midiaAlterando);
+                } else {
+                    Musica musicaAlterando = null;
+                    try (FileInputStream fis = new FileInputStream(caminhoArquivo);
+                         ObjectInputStream ois = new ObjectInputStream(fis)) {
+
+                        midiaAlterando = (Midia) ois.readObject();
                     }
 
-                    sv.atualizarMidia(midiaAlterando);
-
-                    return true;
-                }
-            } catch (ArquivoNaoExisteExcecao a) {
-                JOptionPane.showMessageDialog(null, a.getMessage());
-                
-            } catch (FileNotFoundException e) {
-                throw new RuntimeException(e);
-                
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-
-            } catch (ClassNotFoundException e) {
-                throw new RuntimeException(e);
-            }
-            return false;
-        }
-
-        public boolean alterarMidia(String caminhoArquivo, float tamanho, double duracao, ETipoArquivo eTipoArquivo, String autorOuArtista, boolean eLivro) throws ArquivoNaoExisteExcecao, CampoVazioOuNuloExcecao, CampoMenorOuIgualAZeroExcecao {
-            try {
-                if (Utilitario.arquivoExiste(caminhoArquivo)) {
-
-                    Midia midiaAlterando;
-
-                    if (eLivro) {
-                        Livro livroAlterando = null;
-
-                        try (FileInputStream fis = new FileInputStream(caminhoArquivo);
-                             ObjectInputStream ois = new ObjectInputStream(fis)) {
-
-                            midiaAlterando = (Midia) ois.readObject();
-                        }
-
-                        if (midiaAlterando instanceof Livro) {
-                            livroAlterando =  (Livro) midiaAlterando;
-                        } else {
-                            return false;
-                        }
-
-                        if (tamanho > 0) {
-                            livroAlterando.setTamanho(tamanho);
-                        }
-
-                        if (duracao > 0) {
-                            livroAlterando.setDuracao(duracao);
-                        }
-
-                        if (!autorOuArtista.isBlank()) {
-                            livroAlterando.setAutor(autorOuArtista);
-                        }
-
-                        if (eTipoArquivo != null) {
-                            livroAlterando.setTipoArquivo(eTipoArquivo);
-                        }
-
+                    if (midiaAlterando instanceof Musica) {
+                        musicaAlterando = (Musica) midiaAlterando;
                     } else {
-                        Musica musicaAlterando = null;
-                        try (FileInputStream fis = new FileInputStream(caminhoArquivo);
-                             ObjectInputStream ois = new ObjectInputStream(fis)) {
-
-                            midiaAlterando = (Midia) ois.readObject();
-                        }
-
-                        if (midiaAlterando instanceof Musica) {
-                            musicaAlterando =  (Musica) midiaAlterando;
-                        } else {
-                            return false;
-                        }
-
-                        if (tamanho > 0) {
-                            musicaAlterando.setTamanho(tamanho);
-                        }
-
-                        if (duracao > 0) {
-                            musicaAlterando.setDuracao(duracao);
-                        }
-
-                        if (!autorOuArtista.isBlank()) {
-                            musicaAlterando.setArtista(autorOuArtista);
-                        }
-
-                        if (eTipoArquivo != null) {
-                            musicaAlterando.setTipoArquivo(eTipoArquivo);
-                        }
+                        return false;
                     }
 
-                    try (FileOutputStream fos = new FileOutputStream(caminhoArquivo);
-                         ObjectOutputStream oos = new ObjectOutputStream(fos)) {
-                        oos.writeObject(midiaAlterando);
+                    if (tamanho > 0) {
+                        musicaAlterando.setTamanho(tamanho);
                     }
 
-                    sv.atualizarMidia(midiaAlterando);
+                    if (duracao > 0) {
+                        musicaAlterando.setDuracao(duracao);
+                    }
 
-                    return true;
+                    if (!autorOuArtista.isBlank()) {
+                        musicaAlterando.setArtista(autorOuArtista);
+                    }
+
+                    if (eTipoArquivo != null) {
+                        musicaAlterando.setTipoArquivo(eTipoArquivo);
+                    }
                 }
-            } catch (ArquivoNaoExisteExcecao a) {
-                JOptionPane.showMessageDialog(null, a.getMessage());
 
-            } catch (FileNotFoundException e) {
-                throw new RuntimeException(e);
+                try (FileOutputStream fos = new FileOutputStream(caminhoArquivo);
+                     ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+                    oos.writeObject(midiaAlterando);
+                }
 
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+                sv.atualizarMidia(midiaAlterando);
 
-            } catch (ClassNotFoundException e) {
-                throw new RuntimeException(e);
+                return true;
             }
-            return false;
+        } catch (ArquivoNaoExisteExcecao a) {
+            JOptionPane.showMessageDialog(null, a.getMessage());
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return false;
+    }
+
+    public boolean renomearMidia(Midia midia, String novoNomeString) throws ArquivoNaoExisteExcecao {
+
+        if (Utilitario.arquivoExiste(midia.getCaminho())) {
+
+            File arquivo = new File(midia.getCaminho());
+
+            File novoNome = new File(arquivo.getParent(), novoNomeString + ".tpoo");
+
+            arquivo.renameTo(novoNome);
+
+            midia.setCaminho(novoNome.getAbsolutePath());
+
+            return true;
         }
 
+        return false;
 
+    }
 
+    public boolean moverMidia(Midia midia, String novoCaminhoString) {
+        if (Utilitario.arquivoExiste(midia.getCaminho())) {
+
+            File arquivo = new File(midia.getCaminho());
+
+            File novoCaminho = new File(novoCaminhoString, midia.getNome() + ".tpoo");
+
+            arquivo.renameTo(novoCaminho);
+            midia.setCaminho(novoCaminho.getAbsolutePath());
+
+            return true;
+        }
+        return false;
+
+    }
 }
