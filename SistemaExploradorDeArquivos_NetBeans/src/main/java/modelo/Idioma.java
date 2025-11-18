59f3b6a295306662c7d639e2d2b662d932c4b6a7
anaclhostins <analopeshostins@gmail.com>
2025-11-18 10:45:40 -0300

    Documentação e ajuste MP4-musica

--- SistemaExploradorDeArquivos_NetBeans/src/main/java/modelo/Idioma.java
+++ SistemaExploradorDeArquivos_NetBeans/src/main/java/modelo/Idioma.java
@@ -3,9 +3,19 @@ package modelo;
 import excecao.CampoVazioOuNuloExcecao;
 import util.ExcecaoUtil;
 
+/**
+ * Representa um idioma utilizado em filme.
+ * A classe contém apenas o nome do idioma.
+ */
 public class Idioma {
     String nome;
 
+    /**
+     * Cria um novo idioma com o nome informado.
+     *
+     * @param nome - Nome do idioma.
+     * @throws CampoVazioOuNuloExcecao - Se o nome for nulo, vazio ou apenas espaços.
+     */
     public Idioma(String nome)throws CampoVazioOuNuloExcecao {
         setNome(nome);
     }
